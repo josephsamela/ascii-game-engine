2fe8d9869dda5625ff916d9ca7f0497862f7885b
Joseph Samela <josephsamela@gmail.com>
2020-11-13 19:21:11 -0500

    jump, platform and new character sprites

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import time
 from controller import Controller
 from engine import Engine, Sprite
 
-
 class Animation:
     def __init__(self, frames):
         self.frame = 0
@@ -55,26 +54,48 @@ class Character:
         self.animation.next()
         self.texture = self.animation.texture
 
+    def down(self, dist):
+        self.pos_y += dist
+        if self.pos_y >= 12:
+            self.pos_y = 12
+        self.texture = self.animation.texture
+            
+
+class Platform(Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(self, pos_x, pos_y)
+        self.solid = True
+
 class Game:
     def __init__(self):
         self.engine = Engine(24, 68)
 
+        gravity = 1
+        maxfallspeed = 200
+        maxspeed = 80
+        jumpforce = 300
+        acceleration = 10 
+
         # Sprites
         ui = Sprite('ui', 0, 0)
-        character = Character(2, 13)
+        character = Character(2, 8)
         background = Sprite('background', 0, 0)
         cloud = Sprite('cloud', 0, 0)
+        ground = Sprite('ground', 0, 16)
 
         self.engine.ui.sprites.append(ui)
         self.engine.bg.sprites.append(background)
         self.engine.bg.sprites.append(cloud)
         self.engine.fg.sprites.append(character)
+        self.engine.fg.sprites.append(ground)
 
         kb = Controller()
 
         self.engine.tick()
         while True:
             time.sleep(1/60)
+
+            character.down(gravity)
 
             cloud.pos_x += 0.01
             self.engine.tick()
